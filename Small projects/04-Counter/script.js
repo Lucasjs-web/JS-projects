b78d2d457155184ca0719fc8b2a3c3500c5d9bc5
Irae <lucasdevjspro@gmail.com>
2025-05-19 15:47:44 +0200

    update

--- Small projects/04-Counter/script.js
+++
@@ -1,9 +0,0 @@
-let calculation = "";
-
-function counterChange(value) {
-  calculation += value;
-  calcamount();
-}
-function calcamount() {
-  document.querySelector("#counter-h1").textContent = `${calculation}`;
-}
