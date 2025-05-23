d47d846d0d6f9e9c6ba7f80c955c1e3321429994
Irae <lucasdevjspro@gmail.com>
2025-05-23 19:25:20 +0200

    update

---
+++ Website-to-put-whatever-in/script.js
@@ -0,0 +1,30 @@
+const navElement = document.querySelector("nav");
+const navBarLinks = document.querySelectorAll("a");
+const sectionElements = document.querySelectorAll("section");
+
+const removeActiveLinks = () => {
+  navBarLinks.forEach((link) => link.parentElement.classList.remove("active"));
+};
+
+const hideSections = () => {
+  sectionElements.forEach((section) => section.classList.add("hidden"));
+};
+
+navBarLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    removeActiveLinks();
+    hideSections();
+    link.parentElement.classList.add("active");
+    const sectionElement = document.querySelector(link.hash);
+    sectionElement.classList.remove("hidden");
+  });
+});
+
+function fortnite() {
+  let audio = new Audio("Fortnite Victory Royale Sound Effect (HD).mp3");
+
+  let audio2 = new Audio("Record (online-voice-recorder.com).mp3");
+
+  audio.play();
+  audio2.play();
+}
