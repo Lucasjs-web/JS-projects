c8f4f13bbbb0de6674f5e47e9f70e2d02748e8fd
Irae <lucasdevjspro@gmail.com>
2025-05-28 15:22:37 +0200

    update

--- Website-to-put-whatever-in/script.js
+++ Website-to-put-whatever-in/script.js
@@ -1,34 +1,10 @@
-const navElement = document.querySelector("nav");
-const navBarLinks = document.querySelectorAll("a");
-const sectionElements = document.querySelectorAll("section");
-
-const removeActiveLinks = () => {
-  navBarLinks.forEach((link) => link.parentElement.classList.remove("active"));
-};
-
-const hideSections = () => {
-  sectionElements.forEach((section) => section.classList.add("hidden"));
-};
-
-navBarLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    removeActiveLinks();
-    hideSections();
-    link.parentElement.classList.add("active");
-    const sectionElement = document.querySelector(link.hash);
-    sectionElement.classList.remove("hidden");
-  });
-});
-
 function fortnite() {
-  let audio = new Audio("Fortnite Victory Royale Sound Effect (HD).mp3");
-
-  let audio2 = new Audio("Record (online-voice-recorder.com).mp3");
-
-  let omgHidden = document.getElementById("omgFortnite");
-
-  omgHidden.classList.remove("hidden");
-
-  audio.play();
-  audio2.play();
+  if (document.getElementById("omgFortnite").classList.contains("hidden")) {
+    let audio = new Audio("Skeletor running and laughing (Short Version).mp3");
+    audio.play();
+    document.getElementById("omgFortnite").classList.remove("hidden");
+  } else {
+    let audio2 = new Audio("Press the damn button.mp3");
+    audio2.play();
+  }
 }
